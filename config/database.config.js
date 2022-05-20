5f8db56ee04cfe0bc8bbc5148c8a447f1cc8cb9d
MatveyUtkov <103751684+MatveyUtkov@users.noreply.github.com>
2022-05-20 20:18:37 +0600

    Update database.config.js

--- config/database.config.js
+++ config/database.config.js
@@ -1,4 +1,4 @@
 module.exports = {
     url: 'mongodb+srv://user:aisha@cluster0.ppulr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-    //url:'mongodb+srv://beisembiyev:baha91290@cluster0.lxq92.mongodb.net/osekter?retryWrites=true&w=majority'
+    
 }
