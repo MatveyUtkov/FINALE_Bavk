1b1afd7bb2e6e6fe7a67fbb3caffd4da5d7b66ee
MatveyUtkov <103751684+MatveyUtkov@users.noreply.github.com>
2022-05-22 14:30:03 +0600

    Add files via upload

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const request=require('request');
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const bcrypt = require('bcrypt');
+
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 const UserRoute = require('./routes/UserRoute')
@@ -22,6 +23,19 @@ mongoose.connect(dbConfig.url, {
     console.log('Could not connect to the database', err);
     process.exit();
 });
+const postSchema= new mongoose.Schema({
+    game_name:String,
+    game_image:String
+
+})
+const postSchema1= new mongoose.Schema({
+    movie_name:String,
+    movie_rate:String
+
+})
+
+const Post1=mongoose.model('Movie',postSchema1);
+const Post= mongoose.model('Post',postSchema);
 app.get("/", function(req, res){
     res.render("register");
 });
@@ -67,10 +81,18 @@ app.post('/games',(req, res) => {
         let image3=json[2].imgUrl;
         let game4=json[3].title;
         let image4=json[3].imgUrl;
+        for(let i=0;i<4;i++){
+            const post=new Post({
+                game_name:json[i].title,
+                game_image:json[i].imgUrl
+            })
+            post.save();
+        }
         res.send("First game is "+game1+"; Second game is "+game2+"; Third game is "+game3+"; And fourth game is "+game4)
     });
 
 });
+
 app.post('/cinema',(req, res) => {
         let cinemaG=req.body.crypto;
         let api="3555a76b4788127f37bff3ab69d8bf00"
@@ -86,6 +108,13 @@ app.post('/cinema',(req, res) => {
                 var score2=json.results[2].vote_average
                 let rate3=json.results[3].original_title
                 var score3=json.results[3].vote_average
+                for(let i=0;i<4;i++){
+                    const post=new Post1({
+                        movie_name:json.results[i].original_title,
+                        movie_rate:json.results[i].vote_average
+                    })
+                    post.save();
+                }
                 res.send("First movie is "+rate+"with score "+score+"; Second gmovie is "+rate1+"with score "+score1+"; Third movie is "+rate2+"with score "+score2+"; And fourth movie is "+rate3+"with score "+score3)
             });
         })
@@ -120,8 +149,11 @@ app.post('/music', (req, res) => {
         let json = JSON.parse(body)
         console.log(json);
         let music1 = json.playlists.items[0].name;
+        let music2 = json.playlists.items[1].name;
+        let music3 = json.playlists.items[2].name;
+        let music4 = json.playlists.items[3].name;
         console.log(music1);
-        res.send("First game is " + music1)
+        res.send("First music is " + music1)
     });
 });
 let port = process.env.PORT||3000;
